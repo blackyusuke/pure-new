7a82544b6254228e098ef9bf5fa4ce68819fb893
blackyusuke <y.ishii@k-1.ne.jp>
2020-08-31 19:18:37 +0900

    purgecssによるスライダーCSSの追加

--- plugins/vue-awesome-swiper.js
+++ plugins/vue-awesome-swiper.js
@@ -2,4 +2,3 @@ import Vue from 'vue'
 import Swiper from 'swiper/swiper-bundle.esm'
 import getAwesomeSwiper from 'vue-awesome-swiper/dist/exporter'
 Vue.use(getAwesomeSwiper(Swiper))
-import 'swiper/swiper-bundle.css'
